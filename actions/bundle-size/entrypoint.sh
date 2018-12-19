4ba2a22c1d017147eeeee2bad620db942192628d
Outsider <outsideris@gmail.com>
2018-12-19 23:57:01 +0900

    pr1
    Signed-off-by: Outsider <outsideris@gmail.com>

--- actions/bundle-size/entrypoint.sh
+++ actions/bundle-size/entrypoint.sh
@@ -6,7 +6,7 @@ DATA=$(ls -lha build/static/js/*.js  | awk '{print $5,$9}')
 
 URL=$(cat /github/workflow/event.json | jq -r .pull_request._links.comments.href)
 
-curl -d '{\"body\":\"$DATA\"}' \
+curl -d "{\"body\":\"$DATA\"}" \
   -H "Authorization: token $GITHUB_TOKEN" \
   -H "Content-Type: application/json" \
   -X POST "$URL"
